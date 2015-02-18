97024b0877289011b1e71c52a67b7c9c01631f02
Jwcrist <jacecrist@gmail.com>
2015-02-17 21:05:53 -0600

    Update30
    Update30

---
+++ homeworks/Assignment_6/Homework_6.R
@@ -0,0 +1,15 @@
+getwd()
+library(MASS)
+head(USArrests)
+write.csv(USArrests,"USArrests.csv")
+
+# 1. Use the "Reporting and Visualization" presentation, posted on blackboard, as a guide to help you complete your homework.
+# 
+# 2. You may use the USArrest data or your own data to create Reports and Dashboards.
+# 
+# 3. Create 2 Dashboards with at least 2 Tableau Sheets each with a unique visualization.
+# 
+# a. 1 Dashboard should have Murder information (or your own variable 1) and 1 Dashboard should have Rape information (or your own variable 2).  Refer to the Presentation on blackboard.
+# 
+# 4. You will be graded on creativity.  Tableau is a very user friendly tool, with a lot of resources to be able to develop great work with little training.
+
